283075e5560c8048654dcc213d11a3f02008f264
“minahow3” <minahow3@gmail.com>
2024-05-19 16:29:50 +0900

    一覧画面　修正

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,30 @@
-// 一覧画面
-
 import Link from "next/link";
+import styles from "../styles/index.module.css";
 
 const avatars = [
-  { id: 1, name: "Avatar 1", imageUrl: "/avatars/avatar1.png" },
-  { id: 2, name: "Avatar 2", imageUrl: "/avatars/avatar2.png" },
+  { id: 1, name: "Avatar 1", imageUrl: "/data/image/sample1.png" },
+  { id: 2, name: "Avatar 2", imageUrl: "/data/image/sample2.png" },
 ];
 
 export default function Home() {
   return (
     <div>
       <h1>Avatar List</h1>
-      <ul>
+      <ul className={styles.avatarList}>
         {avatars.map((avatar) => (
           <li key={avatar.id}>
             <Link href={`/view/${avatar.id}`}>
-              <a>
+              <div className={styles.avatarContainer}>
+                <p className={styles.avatarName}>{avatar.name}</p>
+
                 <img
                   src={avatar.imageUrl}
                   alt={avatar.name}
                   width={50}
                   height={50}
+                  className={styles.avatarImage}
                 />
-                <p>{avatar.name}</p>
-              </a>
+              </div>
             </Link>
           </li>
         ))}
